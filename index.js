5fa2530072d8a9ecb3cff5018c929d2b41684e30
Livia-Arr <liviapereira0516@gmail.com>
2023-08-22 21:48:51 -0300

    resolvido

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
 console.log("arrays")
 
+//exercicio 1
 //a. undefined
 //b. null
 //c. 11
-//d. 
+//d. 3
+//e. [3, 19, 4... e o resto]
+//f. 9
+
+//exercicio 2
+//SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+//1 escrita e codigo
+/*let emailUser = prompt("Digite seu e-mail");
+let nomeUser = prompt("Digite seu nome de usuário")
+
+console.log(`O e-mail ${emailUser} foi cadastrado com sucesso. Seja bem-vinda(o) ${nomeUser}!`);
+*/
+
+//exercicio 2
+/* let comidasGosto = ["escondidinho", "sonho", "empada", "carbonara", "torta"]
+console.log(comidasGosto)
+
+console.log(`Essas são minhas comidas favoritas ${comidasGosto[0]}`)
+console.log(`Essas são minhas comidas favoritas ${comidasGosto[1]}`)
+console.log(`Essas são minhas comidas favoritas ${comidasGosto[2]}`)
+console.log(`Essas são minhas comidas favoritas ${comidasGosto[3]}`)
+console.log(`Essas são minhas comidas favoritas ${comidasGosto[4]}`)
+
+
+let comidaUser = prompt("Insira sua comida preferida")
+comidasGosto[1] = comidaUser
+
+console.log(comidasGosto)
+*/
+
+//exercicio 3
+/*let listaDeTarefas = []
+let tarefa = prompt("Insira uma tarefa que você concui diariamente")
+let tarefa2 = prompt("Reflita e insira mais uma")
+let tarefa3 = prompt("Só mais uma, por favor!")
+
+listaDeTarefas.push(tarefa)
+listaDeTarefas.push(tarefa2)
+listaDeTarefas.push(tarefa3)
+
+console.log(listaDeTarefas)
+
+let pergunta = prompt("Escolha uma das tarefas: 0, 1 ou 2")
+listaDeTarefas.splice(pergunta)
+console.log(listaDeTarefas)
+*/
+
+/*let digiteFrase = prompt("Monte uma frase. Seja criativo!")
+const aparador = digiteFrase.split(" ")
+let fraseSeparada = []
+fraseSeparada.push(aparador)
+console.log(fraseSeparada)
+*/
+
+
